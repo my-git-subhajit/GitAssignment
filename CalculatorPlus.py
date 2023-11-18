8b18742e8c2e5830277c009094f730b384b130bd
my-git-subhajit <130735403+my-git-subhajit@users.noreply.github.com>
2023-11-18 18:37:56 +0530

    code changes

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -16,13 +16,11 @@ class Calculator:
 
     def divide(self, a, b):
 
-        if b == 0:
-
-            raise ValueError("Cannot divide by zero.")
-
-
         return a / b
-    
+   
+    def square_root(self, x):
+
+        return math.sqrt(x)
     
     
 if __name__ == "__main__":
@@ -33,6 +31,8 @@ if __name__ == "__main__":
 
     num2 = 4
 
+    num3 = 25
+
     print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
 
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
@@ -40,3 +40,5 @@ if __name__ == "__main__":
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+
+    print(f"The square root of {num3} = {calculator.square_root(num3)}")
